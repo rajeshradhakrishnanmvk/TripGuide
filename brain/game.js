c8015071b68f0d2192d7bed840ef68098af0731c
Rajesh Radhakrishnan <rajeshmvk@gmail.com>
2024-04-10 22:23:16 +0530

    connections integrated with gemini pro 1.5

--- brain/game.js
+++ brain/game.js
@@ -1,5 +1,51 @@
-let gl_pattern,gl_solutions,gl_meanings ; // Declare a global variable
+let gl_pattern,gl_solutions,gl_meanings, gamecount=1, currentPage=1; // Declare a global variable
 
+const fetchConnectionsFromStorage = async (key) => {
+   
+    const pattern = [];
+    const solutions = [];
+    const meanings = [];
+    console.log("fetchConnectionsFromStorage: ",'gemma-' + key);
+    var data = getFromLocalStorage('gemma-' + key);
+    data = data.toLowerCase(); // Convert data to lowercase
+    data = JSON.parse(cleanJson(data));
+    if (data && data.patterns) {
+        data = data.patterns;
+    } 
+    console.log(data);
+    // Assuming the JSON has an array of objects with pattern, solutions, and meanings properties
+    if (data && data.length > 0) {
+        data.forEach(obj => {
+            if (Array.isArray(obj.japanese)) {
+                pattern.push(obj.pattern.slice(0, 4));
+            } else {
+                pattern.push(obj.pattern.split(", ").map(item => item.trim()).slice(0, 4));
+            }
+            if (Array.isArray(obj.japanese)) {
+                solutions.push(obj.japanese.slice(0, 4));
+            } else {
+                solutions.push(obj.japanese.split(", ").map(item => item.trim()).slice(0, 4));
+            }
+            if (Array.isArray(obj.english)) {
+                meanings.push(obj.english.slice(0, 4));
+            } else {
+                meanings.push(obj.english.split(", ").map(item => item.trim()).slice(0, 4));
+            }
+        });
+    }
+    // console.log(pattern);
+    // console.log(solutions);
+    // console.log(meanings);
+    return { pattern, solutions, meanings };
+};
+// Function to remove unnecessary text from JSON data
+function cleanJson(jsonString) {
+    // Define regular expression to remove triple backticks and 'json' keyword
+    const regex = /```|json/g;
+    // Replace unnecessary text with an empty string
+    const cleanedJson = jsonString.replace(regex, '');
+    return cleanedJson;
+}
 const fetchConnectionsFromDisk_v1 = async (fileNumber) => {
    
     const pattern = [];
@@ -7,20 +53,33 @@ const fetchConnectionsFromDisk_v1 = async (fileNumber) => {
     const meanings = [];
     const fileName = 'patterns\\batch-'+fileNumber+'.json';
     const response = await fetch(fileName); // Fetch the file
-    const data = await response.json(); // Parse the JSON
+    var data = await response.json(); // Parse the JSON
+    data = JSON.parse(cleanJson(data)).patterns;
     // Assuming the JSON has an array of objects with pattern, solutions, and meanings properties
-    data.forEach(obj => {
-        pattern.push(obj.pattern);
-        solutions.push(obj.japanese);
-        meanings.push(obj.english);
-    });
+    if (data && data.length > 0) {
+        data.forEach(obj => {
+            if (Array.isArray(obj.japanese)) {
+                pattern.push(obj.pattern);
+            } else {
+                pattern.push(obj.pattern.split(", ").map(item => item.trim()));
+            }
+            if (Array.isArray(obj.japanese)) {
+                solutions.push(obj.japanese);
+            } else {
+                solutions.push(obj.japanese.split(", ").map(item => item.trim()));
+            }
+            if (Array.isArray(obj.japanese)) {
+                meanings.push(obj.english);
+            } else {
+                meanings.push(obj.english.split(", ").map(item => item.trim()));
+            }
+        });
+    }
     // console.log(pattern);
     // console.log(solutions);
     // console.log(meanings);
     return { pattern, solutions, meanings };
 };
-
-// Function to shuffle a two-dimensional array
 function shuffleTwoDimensionalArray(arr) {
     const flatArr = arr.flat(); // Flatten the array
     flatArr.sort(() => Math.random() - 0.5); // Shuffle the flattened array
@@ -42,13 +101,15 @@ function shuffleTwoArrays(A, B) {
     // Shuffle arrays A and B while preserving relationships between corresponding elements
     const shuffledA = shuffleTwoDimensionalArray(A);
     const shuffledB = shuffleTwoDimensionalArray(B);
-    console.log(shuffledA);
-    console.log(shuffledB);
+    //console.log(shuffledA);
+    //console.log(shuffledB);
     return [shuffledA, shuffledB];
 }
 
-function fetchConnectionsFromDisk() {
 
+function fetchConnectionsFromDisk() {
+    gamecount = Object.keys(localStorage).filter(key => key.startsWith('gemma-')).length;
+    console.log("fetchConnectionsFromDisk: ",gamecount);
     backend(1)
 
     
@@ -68,10 +129,29 @@ function showGridLoading(isLoading) {
         }
     }
 }
+const fetchDinkanPrompt = async () => {
+    const fileName = 'jn.txt'
+    const response = await fetch(fileName); 
+    return await response.text();
+}
+function dinkan(){
+    showGridLoading(true);
+    fetchDinkanPrompt().then(result => {
+            fetchConnectionsFromGemma(result).then((gemmaresponse) => {
+                console.log('Data fetched from Gemma!');
 
+
+            }).catch(error => {
+                console.error('Error:', error);
+            });
+        }).catch(error => {
+            // Handle any errors here
+            console.error('Error:', error);
+        }
+    );
+    showGridLoading(false);
+}
 function boardSetup() {
-    // const prompt = fs.readFileSync('/d:/TripGuide/brain/gold.txt', 'utf-8');
-    // fetchConnectionsFromGemma(prompt);
     let clickedPositions = [];
     const gameBoard = document.querySelector('.game-board');
     const rows = gameBoard.querySelectorAll('.row');
@@ -105,9 +185,9 @@ function boardSetup() {
     console.log('Board setup!');
 }   
 
-function fetchConnectionsFromGemma(examples_prompt) {
-    showGriLoading(true);
-    fetch('https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=', {
+async function fetchConnectionsFromGemma(examples_prompt) {
+   
+    const response = await fetch('https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -125,20 +205,50 @@ function fetchConnectionsFromGemma(examples_prompt) {
             ]
         })
     });
+    gamecount++;
+    const data = await response.json();
+    //console.log(data.candidates[0].content.parts[0].text);
+    saveToLocalStorage('gemma-' + gamecount, data.candidates[0].content.parts[0].text);
+
+}
+// Function to save data to local storage
+function saveToLocalStorage(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+    console.log(`Data saved to local storage with key: ${key}`);
 }
 
-function previousPage(){
-
-    backend(1)
-
+function getFromLocalStorage(key) {
+    const data = localStorage.getItem(key);
+    return JSON.parse(data);
 }
 
-function nextPage(){
-    backend(2)
+function previousPage() {
+    const prevPage = currentPage - 1;
+    console.log("Previous page ",prevPage);
+    if (currentPage > 1 && getFromLocalStorage('gemma-' + prevPage)) {
+        console.log("Fetching previous page...", prevPage)
+        currentPage = prevPage;
+        backend(prevPage);
+    } else {
+        console.log("No previous page available.");
+    }
+}
+
+function nextPage() {
+    const nextPage = currentPage + 1;
+    console.log("Next page ",nextPage);
+    if (currentPage < gamecount  && getFromLocalStorage('gemma-' + nextPage)) {
+        console.log("Fetching next page...", nextPage)
+        currentPage = nextPage;
+        backend(nextPage);
+    } else {
+        console.log("No next page available.");
+    }
 }
 
 function backend(pageNumber){
-    fetchConnectionsFromDisk_v1(pageNumber).then(result => {
+    //fetchConnectionsFromDisk_v1(pageNumber).then(result => {
+    fetchConnectionsFromStorage(pageNumber).then(result => {
         // Use the result here
         showGridLoading(true);
 
