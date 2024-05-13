96ff78fde1e595da64b2aab4f896a3d611312de6
Rajesh Radhakrishnan <rajeshmvk@gmail.com>
2024-05-13 12:49:48 +0530

    RAG openai

---
+++ docChat/openai-rag.py
@@ -0,0 +1,147 @@
+# ref: https://github.com/sugarforever/LangChain-Tutorials/blob/main/LangChain_Google_Gemini_API.ipynb
+#https://nanonets.com/blog/langchain/
+import streamlit as st
+import textwrap
+import os
+import requests
+import json
+
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from htmlTemplates import css, bot_template, user_template
+from langchain.text_splitter import CharacterTextSplitter
+# from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+from langchain.schema import messages_from_dict, messages_to_dict
+from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain, ConversationChain
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def wrap_text_preserve_newlines(text, width=110):
+    # Split the input text into lines based on newline characters
+    lines = text.split('\n')
+    # Wrap each line individually
+    wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
+    # Join the wrapped lines back together using newline characters
+    wrapped_text = '\n'.join(wrapped_lines)
+    return wrapped_text
+
+def get_text_chunks(rtext):
+    
+    # wtext = wrap_text_preserve_newlines(rtext)
+
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(rtext)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=OpenAIEmbeddings())
+    return vectorstore
+
+
+# def send_post_request(text, lang):
+#     url = "http://localhost:6000/indiceng"
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+#     payload = {
+#         "text": text,
+#         "lang": lang
+#     }
+
+#     response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+#     if response.status_code == 200:
+#         print("Request successful!")
+#         print("Response:")
+#         print(response.json())
+#     else:
+#         print(f"Request failed with status code {response.status_code}")
+#     return response.json()["output"]
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(     llm=llm,
+                                                                    retriever=vectorstore.as_retriever(),
+                                                                    memory=memory
+                                                                )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    if st.session_state.conversation is None:
+        st.session_state.conversation = get_conversation_chain(None)  # Initialize conversation chain
+    
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        # Call the function to send the POST request
+        if message.content :
+            #msg = send_post_request(message.content,"Malayalam")
+            if i % 2 == 0:
+                st.write(user_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+        
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                #get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                #get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                #create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                #create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)          
+
+
+
+if __name__ == '__main__':
+    main()
